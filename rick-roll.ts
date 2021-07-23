f634799f1bf02e9492ce88dcc2c5a0671d8b8b2d
stranothus <83613280+stranothus@users.noreply.github.com>
2021-07-23 14:51:00 -0500

    Update rick-roll.ts

--- rick-roll.ts
+++ rick-roll.ts
@@ -1,9 +1,13 @@
-var links:HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName("a");
+var links:HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName("a");//access all links
 
+//access saved Rick Rolls
 chrome.storage.sync.get(["Rick Rolls"], function(result:any) {
+    
+    //loop through links
     for(var i = 0; i < links.length; i++) {
         var index:HTMLAnchorElement = links[i];
-
+        
+        //add "(Rick Roll)" warning to all anchor elements with a link to a saved Rick Roll
         if(result["Rick Rolls"].includes(index.href)) {
             index.innerHTML = index.innerHTML + " (Rick Roll)";
         }
